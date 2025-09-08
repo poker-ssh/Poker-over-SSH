e73596038d7a63a7688cfcbae5ff9989b54be2ce
Raymont Qin <140027854+QinCai-rui@users.noreply.github.com>
2025-09-08 19:57:36 +1200

    Revert "Modulise big files"

--- poker/ui/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-UI module for Poker-over-SSH.
-Provides terminal UI components for consistent presentation.
-"""
-
-from .colors import Colors
-from .cards import card_str, cards_horizontal, SUIT_SYMBOLS, SUIT_COLORS
-
-__all__ = ['Colors', 'card_str', 'cards_horizontal', 'SUIT_SYMBOLS', 'SUIT_COLORS']
